34a5d4cd89ec5190bd03df1fc1c0629158d7e7de
BrittneyM34 <brittneym1018@yahoo.com>
2023-10-27 15:25:26 -0400

    added json movie list

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const http = require('http');
+
+let topTenMovies = [
+    {
+        title: '17 Again',
+        director: 'Burr Steers',
+        yearReleased: '2009',
+    },
+    {
+        title: 'Coraline',
+        director: 'Henry Selick',
+        yearReleased: '2009',
+    },
+    {
+        title: 'Us',
+        director: 'Jordan Peele',
+        yearReleased: '2019',
+    },
+    {
+        title: 'Grown Ups',
+        director: 'Dennis Dugan',
+        yearReleased: '2010',
+    },
+    {
+        title: 'Lilo and Stitch',
+        director: '',
+        yearReleased: '2009',
+    },
+    {
+        title: 'Free Guy',
+        director: 'Shawn Levy',
+        yearReleased: '2021',
+    },
+    {
+        title: 'The Green Mile',
+        director: 'Frank Darabont',
+        yearReleased: '1999',
+    },
+    {
+        title: 'Lone Survivor',
+        director: 'Peter Berg',
+        yearReleased: '2013',
+    },
+    {
+        title: 'The Conjuring',
+        director: 'James Wan',
+        yearReleased: '2013',
+    },
+    {
+        title: 'The Curse of La Llorona',
+        director: 'Michael Chaves',
+        yearReleased: '2019',
+    },
+]
+
+app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Movie Flix')
+});
