8d37faea89dde00afcd9a542ca1e8ed46082bcf2
BrittneyM34 <brittneym1018@yahoo.com>
2023-11-30 11:22:34 -0500

    changed endpoints

--- index.js
+++ index.js
@@ -5,12 +5,31 @@ const express = require("express");
 
 morgan = require('morgan');
 
+let users = [
+   {
+    id:1,
+    name:"Kim",
+    favoriteMovies: []
+   },
+   {
+    id:2,
+    name:"Joe",
+    favoriteMovies:["Free Guy"]
+   }
+]
+
 let movies = [
     {
-        title: '17 Again',
-        director: 'Burr Steers',
-        genre: 'Comedy',
-        featured: 'Yes',
+        "title": '17 Again',
+        "director": {
+            "Name": 'Burr Steers',
+            "Description": "Burr Gore Steers is an American actor, screenwriter, and director. His films include Igby Goes Down and 17 Again.",
+        },
+        "genre":  {
+            "Name": 'Comedy',
+            "Description": "Comedy is a genre of film in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect.",
+        },
+        "featured": 'Yes',
     },
     {
         title: 'Coraline',
@@ -68,11 +87,11 @@ let movies = [
     },
 ]
 
-app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
 
 app.get('/movies', (req, res) => {
-    res.statues(200).json(movies);
+    res.status(200).
+     json(movies);
 });
 
 app.get('/', (req, res) => {
@@ -99,80 +118,49 @@ app.get('/movies', (req, res) => {
         res.status(500).send('Error: ' + err);
     });
 });
-
 //Return data about a single movie by title
-app.get('/movies:/Title', (req, res) =>{
-    movies.findOne({Title: req.params.Title})
-    .then((movie) => {
-        res.status(200).json(movie);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error ' + err);
+    app.get('/movies/:title', (req, res) => {
+        res.json(movies.find((movie) =>
+            { return movie.title === req.params.title }));
     });
-});
 
 //Return data about a genre description by name
-app.get('/movies:/genre:/genreName', (req, res) => {
-    movies.findOne({'Genre.Name': req.params.genreName})
-    .then((movie) => {
-        res.status(200).json(movie.Genre);
-    })
-    .catch((err) => {
-        res.status(500).send('Error ' + err);
+    app.get('/movies/:genre/:genreName', (req, res) => {
+        const { genreName }= req.params;
+        const genre = movies.find( movie => movie.genre.Name === genreName ).genre;
+
+        if (genre) {
+            res.status(200).json(genre);
+        } else {
+            res.status(400).send('No such genre')
+        }
     });
-});
 
 //Return data about a director (bio, birth year, death year) by name
-app.get('/movies:/director:/directorName', (req, res) => {
-    movies.fineOne({'Director.Name': req.params.directorName})
-    .then((movie) => {
-        res.status(200).json(movie.Director);
-    })
-    .catch((err) => {
-        res.status(500).send('Error ' + err);
+    app.get('/movies/:director/:directorName', (req, res) => {
+        const { directorName }= req.params;
+        const director = movies.find( movie => movie.director.Name === directorName ).director;
+
+        if (director) {
+            res.status(200).json(director);
+        } else {
+            res.status(400).send('No such director')
+        }
     });
-});
 
 //Allow new users to register
-app.post('/users', [
-    check('Username', 'Username is required').isLength({min:6}),
-    check('Username', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail(),
-],  (req, res) => {
-    let errors = validationResults(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()});
+app.post('/users', (req, res) => {
+    let newUser = req.body;
+  
+    if (!newUser.name) {
+      const message = 'Missing "name" in request body';
+      res.status(400).send(message);
+    } else {
+      newUser.id = uuid.v4();
+      users.push(newUser);
+      res.status(201).send(newUser);
     }
-
-    let hashedPassword = Users.hashPasswortd(req.body.Password);
-
-    Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-        if (user) {
-            return res.status(400).send(req.body.Username + ' already exists');
-        } else {
-            User 
-            .create({
-                Username: req.body.Username,
-                Password: hashedPassword,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday,
-            })
-            .then((user) =>{res.status(201).json(user)})
-            .catch((error) => {
-                console.error(error);
-                res.status(500).send('Error: ' + error);
-            })
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-    });
-});
+  });
 
 //Allow users to update their user info (username, password, email, date of birth)
 app.put('/users/:Username', (req, res) => {
@@ -248,6 +236,53 @@ app.delete('/users/:Username', (req, res) => {
         console.error(err);
         res.status(500).send('Error ' + err);
     });
-});
+});    
 
-app.listen(8080, () => console.log ("listening on 8080"))
+
+// //Return data about a single movie by title
+// // app.get('/movies/:Title', (req, res) =>{
+//     // movies.findOne({Title: req.params.Title})
+//     // .then((movie) => {
+//     //     res.status(200).json(movie);
+//     // })
+//     // .catch((err) => {
+//     //     console.error(err);
+//     //     res.status(500).send('Error ' + err);
+//     // });
+// // });
+
+// //Return data about a genre description by name
+// app.get('/movies/:genre/:genreName', (req, res) => {
+//     genre.findOne({'Genre.Name': req.params.genreName})
+//     .then((movie) => {
+//         res.status(200).json(movie.Genre);
+//     })
+//     .catch((err) => {
+//         res.status(500).send('Error ' + err);
+//     });
+// });
+
+// //Return data about a director (bio, birth year, death year) by name
+// app.get('/movies/:director/:directorName', (req, res) => {
+//     director.findOne({'Director.Name': req.params.directorName})
+//     .then((movie) => {
+//         res.status(200).json(movie.Director);
+//     })
+//     .catch((err) => {
+//         res.status(500).send('Error ' + err);
+//     });
+// });
+
+// //Allow new users to register
+// app.post('/users', (req, res) => {
+//     let newUser = req.body;
+  
+//     if (!newUser.name) {
+//       const message = 'Missing "name" in request body';
+//       res.status(400).send(message);
+//     } else {
+//       newUser.id = uuid.v4();
+//       users.push(newUser);
+//       res.status(201).send(newUser);
+//     }
+//   });
