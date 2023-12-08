93e6e917dd3ecfdbc7ab88f7f65c39c475de4ff6
BrittneyM34 <brittneym1018@yahoo.com>
2023-12-08 11:47:55 -0500

    fixed

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ app.get('/movies', (req, res) => {
 //Delete a user by username
 app.delete('/users/:Username', async (req, res) => {
     await Users.findOneAndUpdate({ Username: req.params.Username })
-    .then((user) {
+    .then((user) => {
         if (!user) {
         res.status(400).send(req.params.Username + ' was not found ');
         } else { 
