0b752b79aa9be7f8a73be7db1cc86fcd5e6d6731
BrittneyM34 <brittneym1018@yahoo.com>
2023-12-19 10:20:13 -0500

    fixed passport errors

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ require('./passport'); //Your local passport file
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject:user.Usernam, //This is the username you're encoding in the JWT
+        subject:user.username, //This is the username you're encoding in the JWT
         expiresIn: '7d', //This specifies that the token will expire in 7 days
         algorithm: 'HS256' //This is the algorithm used to "sign" or encode the values of the JWT
     });
@@ -19,7 +19,7 @@ module.exports = (router) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
-                    message: 'Something is not right',
+                    message: 'Login failed',
                     user:user
                 });
             }
@@ -28,6 +28,7 @@ module.exports = (router) => {
                     res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
+                res.status(200).json({ token: token});
             });
         })(req, res);
     });
