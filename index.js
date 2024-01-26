8920ffa7b51ac67520139c21446d5630f0506ac7
BrittneyM34 <brittneym1018@yahoo.com>
2024-01-26 15:32:08 -0500

    cors

--- index.js
+++ index.js
@@ -30,18 +30,18 @@ const Users = Models.User;
 
 let allowedOrigins = ['http://localhost:8080", "http://testsite.com", "http://localhost:1234'];
 
-// app.use(cors ({
-//     origin: (origin, callback) => {
-//             if(!origin) return callback (null, true);
-//             if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
-//                 let message = 'The CORS policy for this application doesn\'t allow acces from origin ' + origin; 
-//                     return callback(new Error(message ), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors ({
+    origin: (origin, callback) => {
+            if(!origin) return callback (null, true);
+            if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
+                let message = 'The CORS policy for this application doesn\'t allow acces from origin ' + origin; 
+                    return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-app.use(cors());
+// app.use(cors());
 
 // let users = [
 //    {
