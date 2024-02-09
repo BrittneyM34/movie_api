cf85e9041e694e1686b8e5ff09d70c2d4d8fce55
BrittneyM34 <brittneym1018@yahoo.com>
2024-02-09 12:44:03 -0500

    remove passport /movies

--- index.js
+++ index.js
@@ -63,16 +63,18 @@ app.listen(port, '0.0.0.0', () => {
 });
 
 // Return a list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    await Movies.find()
-        .then((movie) => {
-            res.status(200).json(movie);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
+app.get('/movies',
+    // passport.authenticate('jwt', { session: false }), 
+    async (req, res) => {
+        await Movies.find()
+            .then((movie) => {
+                res.status(200).json(movie);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
+    });
 
 // Return data about a single movie by title
 app.get('/movies/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
